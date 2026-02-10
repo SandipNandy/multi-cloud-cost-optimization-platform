cc556ce163edac14fa2a97faa9b7d1b7a16e1df1
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2026-02-11 00:48:05 +0530

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -1,6 +1,7 @@
 -- Cloud Cost Anomaly Detection Database Schema
 -- This file runs automatically when PostgreSQL container starts
 -- Create database if it doesn't exist
+
 SELECT 'CREATE DATABASE cloud_cost'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cloud_cost')\gexec
 
