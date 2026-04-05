a8077245c76e80f3d81e6c64689f3e39dff971f8
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2026-04-05 11:40:19 +0530

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -9,6 +9,7 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cloud_cost')\gexec
 
 \c cloud_cost;
 
+
 -- Anomalies table
 CREATE TABLE IF NOT EXISTS cost_anomalies (
     id SERIAL PRIMARY KEY,
